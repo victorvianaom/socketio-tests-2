23cc2366851bb86c6861fde5491c55d32565641f
Victor Viana <victorvianaom@gmail.com>
2020-05-22 05:42:36 +0000

    a porra do problema era o 'socket.broadcast.emit' que eu nao saabia

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import http from "http"
 import express from "express"
-import socketio from "socket.io"
+import socketio from "socket.io" //////SOCKETIO
 import path from "path"
 import ejs from "ejs"
 
 const port = 5000
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server) //////SOCKETIO
 
 app.use(express.static('public'))
 app.set('views', 'public')
@@ -18,6 +18,19 @@ app.use('/', (req, res) => {
     res.render('index.html')
 })
 
+let messages = [] 
 
+io.on('connection', (socket) => { //////SOCKETIO
+    console.log(`> Socket Conectado: ${socket.id}`) //////SOCKETIO
 
-server.listen(port)
+    socket.emit('previuosMessages', messages) //////SOCKETIO
+
+    socket.on('sendMessage', (data) => { //////SOCKETIO
+        messages.push(data) //////SOCKETIO
+        socket.broadcast.emit('receivedMessage', data) //////SOCKETIO
+    })
+})
+
+server.listen(port, () => {
+    console.log(`> Listening on port: ${port}`)
+})
